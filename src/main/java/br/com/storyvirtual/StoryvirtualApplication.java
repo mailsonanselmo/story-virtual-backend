865fbdb3a077e7d65b7371a9cc0f047825bb42e9
mailsonanselmo <mailson.anselmo@gmail.com>
2022-12-12 18:37:53 -0300

    Start Project

---
+++ src/main/java/br/com/storyvirtual/StoryvirtualApplication.java
@@ -0,0 +1,13 @@
+package br.com.storyvirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryvirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryvirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/storyvirtual/StoryvirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.storyvirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryvirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
